41c7a941d32454e9179a73d3eea19491aedb9c61
ryaniscool-cyber <ryvn.antony@gmail.com>
2026-02-05 19:26:22 -0700

    Initialize Electron app with main window setup

---
+++ electron.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
